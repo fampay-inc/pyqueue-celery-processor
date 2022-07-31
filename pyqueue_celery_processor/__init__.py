894cc10499d28e0b0320acbff83af0b2fa2791a1
Chirag Maheshwari <chirag@fampay.in>
2022-07-31 20:23:26 +0530

    mark logging false as default behaviour

--- pyqueue_celery_processor/__init__.py
+++ pyqueue_celery_processor/__init__.py
@@ -2,15 +2,17 @@
 Implementation referred from segment analytics-python
 https://github.com/segmentio/analytics-python/tree/master/analytics
 """
+import os
 from .client import Client
+
+__all__ = ["add_to_queue", "Client"]
 
 default_client = None
 
 """SETTINGS"""
-debug = True
-send = True
+debug = bool(os.getenv("PYQUEUE_DEBUG", False))
+send = bool(os.getenv("PYQUEUE_SEND", True))
 max_queue_size = 1_00_000
-__all__ = ["add_to_queue"]
 
 
 def add_to_queue(task_func, countdown=0, *args, **kwargs):
